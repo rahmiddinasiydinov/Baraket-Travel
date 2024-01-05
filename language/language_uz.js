ff0c18912c32fc72f2b0679fcb01f5d42d2cffa7
Rahmiddin Asiydinov <81851084+rahmiddinasiydinov@users.noreply.github.com>
2024-01-05 22:06:45 +0500

    language change in mobile view

--- language/language_uz.js
+++ language/language_uz.js
@@ -1,8 +1,9 @@
-language_uz.addEventListener('change', (e) => {
+const languageSelect = document.querySelectorAll('.language-change-uz')
+languageSelect.forEach(e => e.addEventListener('change', (e) => {
     const value = e.target.value;
+    console.log('hiii')
     if(value ==='ru'){
         window.localStorage.setItem('language', value);
         window.location.href = `../${value}`
     }
-})
-
+}))
